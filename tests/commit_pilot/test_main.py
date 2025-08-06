24564b46a90a3c9ab9541218d51eea946fa587ab
YWT <theabc50111@gmail.com>
2025-08-07 00:14:05 +0800

    [test] add test command for run_command

--- tests/commit_pilot/test_main.py
+++ tests/commit_pilot/test_main.py
@@ -5,9 +5,13 @@ from src.commit_pilot.main import run_command
 
 @pytest.mark.parametrize(
     argnames = "command, extra_args, expected",
-    argvalues = [("echo", "hi~", "hi~\n")],
-    ids = ["echo"]
+    argvalues = [
+        ("echo", ["hi~"], "hi~"),
+        ("git", ["rev-parse", "--git-dir"], ".git")
+    ],
+    ids = ["echo", "check git dir"]
 )
 def test_run_command(command, extra_args, expected):
     res = run_command(command, extra_args)
-    assert res == expected
+    print(res)
+    assert expected in res
